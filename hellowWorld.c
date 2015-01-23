bdd65602cdff17cdae2a749d1b769463633ef089
ugiacoman <ulises.giacoman@gmail.com>
2015-01-23 16:31:27 -0500

    simple hello world

---
+++ hellowWorld.c
@@ -0,0 +1,15 @@
+//
+//  hellowWorld.c
+// 
+//
+//  Created by Ulises Giacoman on 1/23/15.
+//  Copyright (c) 2015 Ulises Giacoman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("hello, world!\n");
+    return 0;
+}
